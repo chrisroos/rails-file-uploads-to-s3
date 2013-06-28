b0c5313cd8a552d6e6a06f7bba572d9e30613634
Chris Roos <chris@seagul.co.uk>
2013-06-28 13:28:18 +0100

    Initial commit - Empty Rails 3.2.13 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsUploadsToS3::Application.config.secret_token = 'f25aa8f041ee40e1bc655c9c8c1ad8cd0997862c73d7a5617dac3837c92e3276d14924471d9eb894ac12e0047a51ee2c2bce86e4cd934519a5caf824a30492e5'
