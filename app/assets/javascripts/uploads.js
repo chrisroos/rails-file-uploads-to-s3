1f9de3e236e5664161f12458044cc19bd8d3f721
Chris Roos <chris@seagul.co.uk>
2013-06-28 17:06:31 +0100

    Add basic progress bar
    Taken from https://github.com/blueimp/jQuery-File-Upload/wiki/Basic-plugin

--- app/assets/javascripts/uploads.js
+++ app/assets/javascripts/uploads.js
@@ -1,3 +1,11 @@
 $(function() {
-  $('form.new_upload').fileupload({});
+  $('form.new_upload').fileupload({
+    progressall: function (e, data) {
+      var progress = parseInt(data.loaded / data.total * 100, 10);
+      $('#progress .bar').css(
+          'width',
+          progress + '%'
+      );
+    }
+  });
 })
